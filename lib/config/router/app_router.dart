3369141424d4d9d72b42f62c7e22b94e694b9426
jocnn <ijann_@outlook.com>
2023-10-03 17:24:02 -0300

    v123 se definió las rutas con go_router

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,20 @@
+import 'package:d_using_widgets_git/presentation/screens/screens.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/buttons',
+      builder: (context, state) => const ButtonsScreen(),
+    ),
+    GoRoute(
+      path: '/cards',
+      builder: (context, state) => const CardsScreen(),
+    ),
+  ],
+);
