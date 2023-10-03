c60dbd2f6de657a8eb35fdd48ddf14a8c24f6142
jocnn <ijann_@outlook.com>
2023-10-03 17:26:35 -0300

    v123 se importó y definió el contexto de empuje

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:d_using_widgets_git/config/menu/menu_items.dart';
-import 'package:d_using_widgets_git/presentation/screens/buttons/buttons_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -55,11 +55,7 @@ class _CustomListTile extends StatelessWidget {
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
       onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const ButtonsScreen(),
-          )
-        );
+        context.push(menuItem.link);
       },
     );
   }
