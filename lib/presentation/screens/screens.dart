250941fb4e844e65011e4822c9a31615479dfa3a
jocnn <ijann_@outlook.com>
2023-10-03 17:23:21 -0300

    v123 se definió el archivo de exportación de rutas

---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:d_using_widgets_git/presentation/screens/cards/cards_screen.dart';
+export 'package:d_using_widgets_git/presentation/screens/buttons/buttons_screen.dart';
+export 'package:d_using_widgets_git/presentation/screens/home/home_screen.dart';
