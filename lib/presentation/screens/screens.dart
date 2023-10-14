d12d026352a622a5d480c1c5873bb2cbc8521bb1
jocnn <ijann_@outlook.com>
2023-10-14 12:27:41 -0300

    v130 se declaró las pantallas faltantes y se exporto

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
 export 'package:d_using_widgets_git/presentation/screens/cards/cards_screen.dart';
 export 'package:d_using_widgets_git/presentation/screens/buttons/buttons_screen.dart';
 export 'package:d_using_widgets_git/presentation/screens/home/home_screen.dart';
+export 'package:d_using_widgets_git/presentation/screens/animated/animated_screen.dart';
+export 'package:d_using_widgets_git/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:d_using_widgets_git/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:d_using_widgets_git/presentation/screens/progress/progress_screen.dart';
+export 'package:d_using_widgets_git/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:d_using_widgets_git/presentation/screens/ui_controls/ui_controls_screen.dart';
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UIControlsScreen extends StatelessWidget {
+  const UIControlsScreen({super.key});
+
+  static const name = 'ui_control_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
