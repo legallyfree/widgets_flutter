aef5b0ea5b464589cefa74f085ef86659ec3e2e0
jocnn <ijann_@outlook.com>
2023-10-17 18:34:45 -0300

    v136 se implementó indicador de progreso

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -7,6 +7,65 @@ class ProgressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Progress Indicator"),
+      ),
+      body: const _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30),
+          Text("Circular progress indicator"),
+          SizedBox(height: 20),
+          CircularProgressIndicator(strokeWidth: 2),
+          SizedBox(height: 20),
+          Text("Circular progress indicator controlado"),
+          _ControllerProgressIndicator(),
+          SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<double>(
+        stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+          return (value * 2) / 100;
+        }).takeWhile((e) => e < 100),
+        builder: (context, snapshot) {
+
+          final progressValue = snapshot.data ?? 0;
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                CircularProgressIndicator(
+                  value: progressValue,
+                  strokeWidth: 2,
+                  backgroundColor: Colors.black12,
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                    child: LinearProgressIndicator(value: progressValue))
+              ],
+            ),
+          );
+        });
   }
 }
