62d05d0557b70759d65719824966f3aebca62ff7
jocnn <ijann_@outlook.com>
2023-11-26 20:22:26 -0300

    v149 se mostró el indicador de refresco

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -32,8 +32,8 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
 
   @override
   void dispose() {
-    isMounted = false;
     scrollController.dispose();
+    isMounted = false;
     super.dispose();
   }
 
@@ -46,6 +46,23 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
     isLoading = false;
 
     if (!isMounted) return;
+    setState(() {});
+  }
+
+  Future<void> onRefresh() async {
+    isLoading = true;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 3));
+    if (!isMounted) return;
+
+    isLoading = false;
+    final lastId = imagesIds.last;
+    imagesIds.clear();
+    imagesIds.add(lastId + 1);
+
+    addFiveImages();
+
     setState(() {});
   }
 
@@ -62,30 +79,34 @@ class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-          controller: scrollController,
-          itemCount: imagesIds.length,
-          itemBuilder: (context, index) {
-            return FadeInImage(
-              fit: BoxFit.cover,
-              width: double.infinity,
-              height: 300,
-              placeholder: const AssetImage('assets/images/jar-loading.gif'),
-              image: NetworkImage(
-                  'https://picsum.photos/id/${imagesIds[index]}/500/300'),
-            );
-          },
+        child: RefreshIndicator(
+          edgeOffset: 10,
+          strokeWidth: 2,
+          onRefresh: onRefresh,
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: imagesIds.length,
+            itemBuilder: (context, index) {
+              return FadeInImage(
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: 300,
+                placeholder: const AssetImage('assets/images/jar-loading.gif'),
+                image: NetworkImage(
+                    'https://picsum.photos/id/${imagesIds[index]}/500/300'),
+              );
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => context.pop(),
+          onPressed: () => context.pop(),
           child: isLoading
               ? SpinPerfect(
                   infinite: true,
                   child: const Icon(Icons.refresh_rounded),
                 )
-              : const Icon(Icons.arrow_back_ios_new_outlined)
-      ),
+              : const Icon(Icons.arrow_back_ios_new_outlined)),
     );
   }
 }
