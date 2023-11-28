5b8753fff308550a7a3e42a8dcf810f7de48cb28
jocnn <ijann_@outlook.com>
2023-11-28 16:41:12 -0300

    v160 se agregó riverpod y el providerscope

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:d_using_widgets_git/config/router/app_router.dart';
 import 'package:d_using_widgets_git/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
