1520779d2f4c28e312b8f924e0ae35046e921ca5
jocnn <ijann_@outlook.com>
2023-11-30 13:15:34 -0300

    v163 se modificó el provider de color y brillo

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,6 +1,8 @@
 import 'package:d_using_widgets_git/config/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final isDarkProvider = StateProvider((ref) => true);
+final isDarkProvider = StateProvider((ref) => false);
 
 final colorListProvider = Provider((ref) => colorList);
+
+final selectedColorProvider = StateProvider((ref) => 0);
--- lib/presentation/screens/theme/theme_screen.dart
+++ lib/presentation/screens/theme/theme_screen.dart
@@ -19,7 +19,9 @@ class ThemeScreen extends ConsumerWidget {
             icon: Icon(isDarkMode
                 ? Icons.light_mode_outlined
                 : Icons.dark_mode_outlined),
-            onPressed: () {},
+            onPressed: () {
+              ref.read(isDarkProvider.notifier).update((state) => !state);
+            },
           )
         ],
       ),
@@ -34,6 +36,7 @@ class _ThemeChangerView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final List<Color> colors = ref.watch(colorListProvider);
+    final int selectedColor = ref.watch(selectedColorProvider);
 
     return ListView.builder(
       itemCount: colors.length,
@@ -44,8 +47,10 @@ class _ThemeChangerView extends ConsumerWidget {
           subtitle: Text('${color.value}'),
           activeColor: color,
           value: index,
-          groupValue: 4,
-          onChanged: (value) {},
+          groupValue: selectedColor,
+          onChanged: (value) {
+            ref.read(selectedColorProvider.notifier).state = index;
+          },
         );
       },
     );
