a9ade69f53cf058a885d4e03b81c863e9cd8b537
jocnn <ijann_@outlook.com>
2023-11-30 13:18:32 -0300

    v163 se cambió el color y brillo de toda la app

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -13,18 +13,20 @@ const colorList = <Color>[
 ];
 
 class Apptheme {
-  Apptheme({this.selectedColor = 0})
+  Apptheme({this.selectedColor = 0, this.isDarkMode = false})
       : assert(selectedColor >= 0, "Selected color must be greater then 0"),
-        assert(selectedColor < colorList.length, "Selected color must be less than ${colorList.length-1}");
+        assert(selectedColor < colorList.length,
+            "Selected color must be less than ${colorList.length - 1}");
 
   final int selectedColor;
+  final bool isDarkMode;
 
   ThemeData getTheme() => ThemeData(
-    useMaterial3: true,
-        brightness: Brightness.light,
-    colorSchemeSeed: colorList[selectedColor],
-    appBarTheme: const AppBarTheme(
-      centerTitle: true,
-    ),
-  );
+        useMaterial3: true,
+        brightness: isDarkMode ? Brightness.dark : Brightness.light,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+        ),
+      );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:d_using_widgets_git/config/router/app_router.dart';
 import 'package:d_using_widgets_git/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'presentation/providers/theme_provider.dart';
 
 void main() {
   runApp(
@@ -11,16 +12,20 @@ void main() {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final bool darkMode = ref.watch(isDarkProvider);
+    final int colorSelected = ref.watch(selectedColorProvider);
+
     return MaterialApp.router(
       title: "Flutter Widgets",
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: Apptheme(selectedColor: 8).getTheme(),
+      theme: Apptheme(selectedColor: colorSelected, isDarkMode: darkMode).getTheme(),
     );
   }
 }
